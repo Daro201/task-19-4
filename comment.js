c8d12d7580a38ae09f30a71ad9f23e8f850c2466
Adrian Meller <melleradria@gmail.com>
2018-08-29 15:26:57 +0200

    Add update

--- comment.js
+++ comment.js
@@ -1,8 +1,10 @@
-import {ADD_COMMENT} from './actions';
-import {REMOVE_COMMENT} from './actions';
-import {EDIT_COMMENT} from './actions';
-import {THUMB_UP_COMMENT} from './actions';
-import {THUMB_DOWN_COMMENT} from './actions';
+import {
+    ADD_COMMENT,
+    REMOVE_COMMENT,
+    EDIT_COMMENT,
+    THUMB_UP_COMMENT,
+    THUMB_DOWN_COMMENT,
+} from './actions';
 
 const initialState = {
     comments: [],
@@ -17,26 +19,26 @@ function comments(state = [], action) {
                 text: action.text
                 votes: 0
             }
-            , ...state.comments];
+            , ...comments];
 
         case REMOVE_COMMENT:
-            return comments: state.comments.filter(comment => comment.id !== action.id);
+            return comments: comments.filter(comment => comment.id !== action.id);
         case EDIT_COMMENT:
-            return comments: state.comments.map(comment => {
+            return comments: comments.map(comment => {
             	if(comment.id === action.id) {
             	return {...comment, text: action.text} 
             	}  
             return comment;
         	});
     	case THUMB_UP_COMMENT:
-            return comments: state.comments.map(comment => {
+            return comments: comments.map(comment => {
                 if(comment.id === action.id) {
                 return {...comment, votes: ++comment.votes}
                 }
             return comment;
             });
         case THUMB_DOWN_COMMENT:
-            return comments: state.comments.map(comment => {
+            return comments: comments.map(comment => {
                 if(comment.id === action.id) {
                 return {...comment, votes: --comment.votes}
                 }
